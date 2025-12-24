8c32fae19def56247ad6ba8342f686473a6c4ddb
Juanpedrogomezespinosa <juanpedrogomezespinosa@gmail.com>
2025-12-24 04:40:57 +0100

    Configurar adaptador Vercel y output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
+  // Añade esta línea para ser explícito:
+  output: "server",
+
   integrations: [react()],
 
   vite: {
